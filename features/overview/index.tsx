737bab4e52d00568bfbe0c239fe2e79b5ac13104
PraiseImmanuel <praiseimmanuel601@gmail.com>
2023-12-15 02:43:17 +0100

    new: Implement Header and Sidebar

---
+++ features/overview/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../layout/header/header";
+import Searchbar from "../layout/header/components/searchbar";
+
+const Overview = () => {
+  return (
+    <>
+      <Header>
+        <Searchbar />
+      </Header>
+    </>
+  );
+};
+
+export default Overview;
